5be401d2abd0e103fffcee2a1479c9ace366f216
alinaaksanova <aksyanova.alina@gmail.com>
2024-10-07 21:00:16 +0300

    добавила docstrings и несколько тестов

--- src/lab1/calculator.py
+++ src/lab1/calculator.py
@@ -1,19 +1,34 @@
+"""
+Функции:
+1) Main - считывание вводимых данных
+2) Calc - сам калькулятор
+"""
+
+
 def main():
-    znak = input('Выберите знак операции(+, -, *, /): ')
-    chislo1 = float(input('a = '))
-    chislo2 = float(input('b = '))
+    """
+    Ввод знака операции
+      и чисел a и b
+    """
+    znak = input("Выберите знак операции(+, -, *, /): ")
+    chislo1 = float(input("a = "))
+    chislo2 = float(input("b = "))
     print(calc(znak, chislo1, chislo2))
 
+
 def calc(znak, chislo1, chislo2):
-    if znak == '+':
+    """
+    Вычисление калькулятором
+     заданного выражения
+    """
+    if znak == "+":
         return chislo1 + chislo2
-    if znak == '-':
+    if znak == "-":
         return chislo1 - chislo2
-    if znak == '*':
+    if znak == "*":
         return chislo1 * chislo2
-    if znak == '/':
+    if znak == "/":
         if chislo2 != 0:
             return chislo1 / chislo2
-        return 'Деление на ноль!'
-
-    return 'Неверный знак операции!'
+        return "Деление на ноль!"
+    return "Неверный знак операции!"
--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -6,7 +6,9 @@ class CalculatorTestCase(unittest.TestCase):
     # Тест для проверки работы, можно удалить
     def test_lab1(self):
         self.assertEqual(calc('+',10,43),53.0)
+        self.assertEqual(calc('+', 41000, 358), 41358.0)
         self.assertEqual(calc('-', 158, 58), 100.0)
         self.assertEqual(calc('/', 24, 0), 'Деление на ноль!')
         self.assertEqual(calc('/', 10, 2), 5.0)
         self.assertEqual(calc('*', 4, 5), 20)
+        self.assertEqual(calc('*', 7, 0), 0)
